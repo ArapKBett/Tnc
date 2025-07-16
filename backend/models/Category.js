2e990d5061da966366f539c336d293cb3f5f0a14
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-16 15:12:43 +0300

    Update Category.js

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,8 +1,25 @@
-const mongoose = require('mongoose');
-const categorySchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  entries: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Entry' }],
-  createdBy: mongoose.Schema.Types.ObjectId,
-});
-module.exports = mongoose.model('Category', categorySchema);
+const mongoose = require("mongoose");
+
+const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  entries: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Entry",
+  }],
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: false,
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Category", CategorySchema);
