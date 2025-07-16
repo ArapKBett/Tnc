caf7c2c4dc815953d94091480a32dcd1cf3d754c
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-16 15:11:49 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,28 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  role: { type: String, enum: ['writer', 'animator', 'viewer'] },
-  createdBy: mongoose.Schema.Types.ObjectId, // only two people can create users
-});
-module.exports = mongoose.model('User', userSchema);
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["writer", "animator", "viewer"],
+    default: "viewer",
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: false,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", UserSchema);
