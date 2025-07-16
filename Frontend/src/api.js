f6ae62f3d595d9eedb167e14053a50905820ade4
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-16 16:49:15 +0300

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api", // Proxy to backend
+  baseURL: process.env.REACT_APP_API_URL || '/api', // Proxy to backend
   headers: {
     "Content-Type": "application/json",
   },
